c8fe7cecc935395a441edf16017ee19c63aade7b
wc-pei <liming.pei@webcrew.co.jp>
2023-03-24 14:35:19 +0900

    add:トップに戻るボタン&処理作成

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,15 @@
 import '../assets/sass/normalize.scss';
 import '../assets/sass/style.scss';
 
+const topBtn = document.getElementById("js-top-scroll")!;
+topBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+window.addEventListener("scroll", () => {
+    const scrollPosition = window.pageYOffset;
+    scrollPosition > 1400 ? topBtn.classList.add("js-show") : topBtn.classList.remove("js-show");
+});
